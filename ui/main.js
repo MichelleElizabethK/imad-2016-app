f5ea4fe72d82828200c05df3dbca6be51e18f884
MichelleElizabethK <michelle.elizabethk@gmail.com>
2016-09-30 20:40:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=document.getELementById('main-text');
-var img=document.getElementById('img');
-img.onclick=function() {
-    img.style.marginleft='100px';
+var img=document.getElementById('madi');
+madi.onclick=function() {
+    madi.style.marginleft='100px';
 }
